cec463236b08c7f95a25de3f28a2fd0cd567065e
TalDoLima <alan.silva397@etec.sp.gov.br>
2023-02-24 21:29:27 -0300

    rotas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Botões com link</title>
+        <p style= "color : green">HOME</p>
+        
+        <div>
+        <a href="contato"><button><b>contato</b></button></a>
+        </div>
+        <br> 
+        <div>
+        <a href="empresa"><button><b>empresa</b></button></a>
+        </div>
+        <br> 
+        <div>
+        <a href="produtos"><button><b>produtos</b></button></a>
+        </div>
+        <br> 
+        <div>
+        <a href="home"><button><b>home</b></button></a>
+        </div>
+        <br> 
+    </head>
+    <body class="antialiased">
+        
+    </body>
+</html>
